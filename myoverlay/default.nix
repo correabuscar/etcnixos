8d1643504cec9c51832004d96d566b7a1c57ce3e
correabuscar <correabuscar+nixosvm1root@gmail.com>
2024-04-03 23:36:05 +0200

    make a dynlib & a bin in rust, as nix derivations
    make the bin use the .so lib

--- myoverlay/default.nix
+++ myoverlay/default.nix
@@ -263,6 +263,49 @@ self: super:
 #      #./patches/systemd_30263.patch #removed from disk
 #      ];
 #  });
+  mybin = super.rustPlatform.buildRustPackage rec {
+    name = "mybin";
+    version = "0.1.0";
+    src = ./rustdylibusage/mybin/mybin-${version}.tar.gz;
+    #cargoSha256 = "0qbdcw601m09fw7vqmafvw5q7w17dh37fbbycs6is3rff5qlmbyw"; #mybin
+    #cargoSha256 = "sha256-i9TVX8LydRtaiEF0SAz0dKbdW2wMxrdisXkY293tI0Q";
+    cargoHackageDeps = false;
+    cargoBuildFlags = [ "--release" ];
+    #cargoDepInputs = [ mylib ];
+    buildInputs = [ self.mylib ];
+    sourceRoot = "./";
+    cargoLock = {
+      lockFile = ./rustdylibusage/mybin/Cargo.lock;
+    };
+
+    #To can find the lib:
+    libPath = super.lib.makeLibraryPath [ self.mylib ];
+    fixupPhase = ''
+      patchelf --set-rpath $libPath \
+      $out/bin/mybin
+      '';
+#    fixupPhase = ''
+#      patchelf --add-rpath $mylib/lib/libmylib.so \
+#      $out/bin/mybin
+#      '';
+
+  };
+
+  mylib = super.rustPlatform.buildRustPackage rec {
+    version = "0.1.0";
+    name = "mylib";
+    src = ./rustdylibusage/mylib/mylib-${version}.tar.gz;
+    #cargoSha256 = "1zwcx7637zl6ka058hfgwzmnxln1y592gfwiycxiz65qygxph383"; #mylib
+    #cargoSha256 = "sha256-7AfdCKEh4xTyRir/hmA8I6WYnoPYAQH8NN1Jx19SYDc";
+    cargoHackageDeps = false;
+    cargoBuildFlags = [ "--release" ];
+    sourceRoot = "./";
+    cargoLock = {
+      lockFile = ./rustdylibusage/mylib/Cargo.lock;
+    };
+  };
+
+
 
 } #self/super
 
